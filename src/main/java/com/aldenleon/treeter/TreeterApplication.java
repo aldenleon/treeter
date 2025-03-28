6e579d61fb93668942084fefaac87c96477e773f
alden.leon <alden.leon3@gmail.com>
2025-03-28 09:55:17 +0100

    first commit

---
+++ src/main/java/com/aldenleon/treeter/TreeterApplication.java
@@ -0,0 +1,13 @@
+package com.aldenleon.treeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeterApplication.class, args);
+	}
+
+}
